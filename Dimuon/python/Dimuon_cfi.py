cd0ad6b0252c04515310a8ae4c9416c5341b6444
Shawn Zaleski <ek7121@wayne.edu>
2016-07-18 21:00:17 +0200

    added lepton ID tag for cfg

--- Dimuon/python/Dimuon_cfi.py
+++ Dimuon/python/Dimuon_cfi.py
@@ -2,8 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 dimuon = cms.EDAnalyzer('Dimuon',
                       genPartsTag=cms.InputTag("genParticles"),
-                      decayParticlePID=cms.int32(13),
+                      decayParticlePID=cms.int32(11),
                       isCI=cms.bool(False),
                       debug=cms.bool(False),
                       status=cms.int32(23),
+                      leptonId=cms.int32(13),
 )
