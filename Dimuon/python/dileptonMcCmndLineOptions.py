3e65e2fcfb9e7a29e69ee5e424fcb4c4876711de
Shawn Zaleski <ek7121@wayne.edu>
2016-07-27 17:56:27 +0200

    added input file name variable

--- Dimuon/python/dileptonMcCmndLineOptions.py
+++ Dimuon/python/dileptonMcCmndLineOptions.py
@@ -6,3 +6,8 @@ def registerDefaultMCOptions(options):
                       VarParsing.VarParsing.multiplicity.singleton,
                       VarParsing.VarParsing.varType.string,
                       "output file name")
+    options.register ('inFile',
+                      "defaultName",
+                      VarParsing.VarParsing.multiplicity.singleton,
+                      VarParsing.VarParsing.varType.string,
+                      "input file name")
