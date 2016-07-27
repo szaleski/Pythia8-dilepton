c3d0dcc3640b7812f6120ed81f44d25d5133c604
Shawn Zaleski <ek7121@wayne.edu>
2016-07-27 17:24:53 +0200

    added for name input for Dilepton_cfg.py

---
+++ Dimuon/python/dileptonMcCmndLineOptions.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+def registerDefaultMCOptions(options):
+    options.register ('filename',
+                      "defaultName",
+                      VarParsing.VarParsing.multiplicity.singleton,
+                      VarParsing.VarParsing.varType.string,
+                      "output file name")
