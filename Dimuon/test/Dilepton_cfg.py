f8e4f1a85fd1ec9440242d48a6158cde3d7904bb
Shawn Zaleski <ek7121@wayne.edu>
2018-02-28 20:20:11 +0100

    update Dilepton_cfg to have variable input name

--- Dimuon/test/Dilepton_cfg.py
+++ Dimuon/test/Dilepton_cfg.py
@@ -20,9 +20,9 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
             # replace 'myfile.root' with the source file you want to use
                             fileNames = cms.untracked.vstring(
-        'root://cmsxrootd.fnal.gov//store/mc/RunIISummer16MiniAODv2/CITo2Mu_M300_CUETP8M1_Lam22TeVConLL_13TeV_Pythia8_Corrected-v3/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/62DF9A66-1B3B-E711-AFFB-3417EBE5291B.root',
-               #                                  'file:$CMSSW_BASE/src/GenStudy/Dimuon/test/%s'%(options.inFile)
-#                                   'root://cmsxrootd.fnal.gov//store/mc/RunIISummer16MiniAODv2/CITo2E_M300_CUETP8M1_Lam10TeVConLL_13TeV-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/E0A1E641-EED8-E611-BE16-0CC47A57D066.root'                               
+#        'root://cmsxrootd.fnal.gov//store/mc/RunIISummer16MiniAODv2/CITo2Mu_M300_CUETP8M1_Lam22TeVConLL_13TeV_Pythia8_Corrected-v3/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/100000/62DF9A66-1B3B-E711-AFFB-3417EBE5291B.root',
+                                                 'file:$CMSSW_BASE/src/GenStudy/Dimuon/test/%s'%(options.inFile)
+#        'root://cmsxrootd.fnal.gov///store/user/szaleski/CITo2Mu_L16TeV_GENSIM17_Test/MuMu_16TeV_GENSIM17_LLConM300/180131_211131/0000/2017test_1.root',
                                 )
                             )
 
